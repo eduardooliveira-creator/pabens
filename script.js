8f7e977fe821b411571b3d5a2837a6a45c639e16
Eduwl <eduardooliveiramoraes99@gmail.com>
2026-08-21 12:18:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ document.getElementById('myButton1').addEventListener('click', function() {
         });
 
         document.getElementById('myButton2').addEventListener('mouseover', function() {
-            var x = Math.floor(Math.random() * 100);
-            var y = Math.floor(Math.random() * 100);
-            document.getElementById('myButton2').style.position = 'absolute';
-            document.getElementById('myButton2').style.left = x + '%';
-            document.getElementById('myButton2').style.top = y + '%';
+                        let contador = 0;
+                        var x = Math.floor(Math.random() * 100);
+                        var y = Math.floor(Math.random() * 100);
+                        document.getElementById('myButton2').style.position = 'absolute';
+                        document.getElementById('myButton2').style.left = x + '%';
+                        document.getElementById('myButton2').style.top = y + '%';
+                               btnAumentar.addEventListener('click', () =>{
+                                    contador++);
+                                    if (contador == 5){
+                                                document.getElementById('buttons').innerText = 'Otario KKKKK';
+                                    }
         });
