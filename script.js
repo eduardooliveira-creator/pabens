30381abba517a9e89babcddb6d20f02bb127533f
eduardooliveira-creator <eduardooliveiramoraes99@gmail.com>
2026-08-21 22:04:33 -0300

    para de clicar

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-document.getElementById('myButton1').addEventListener('click', function() {
-            document.getElementById('buttons').innerText = 'Otario KKKKK';
-        });
-
-        document.getElementById('myButton2').addEventListener('mouseover', function() {
-                        let contador = 0;
-                        var x = Math.floor(Math.random() * 100);
-                        var y = Math.floor(Math.random() * 100);
-                        document.getElementById('myButton2').style.position = 'absolute';
-                        document.getElementById('myButton2').style.left = x + '%';
-                        document.getElementById('myButton2').style.top = y + '%';
-                               btnAumentar.addEventListener('click', () =>{
-                                    contador++);
-                                    if (contador == 5){
-                                                document.getElementById('buttons').innerText = 'Otario KKKKK';
-                                    }
-        });
+document.getElementById('myButton1').addEventListener('click', function () {
+    document.getElementById('buttons').innerText = 'Otario KKKKK';
+});
+let cont = 0;
+document.getElementById('myButton2').addEventListener('mouseover', function () {
+        var x = Math.floor(Math.random() * 100);
+        var y = Math.floor(Math.random() * 100);
+        document.getElementById('myButton2').style.position = 'absolute';
+        document.getElementById('myButton2').style.left = x + '%';
+        document.getElementById('myButton2').style.top = y + '%';
+        cont++;
+        if (cont >= 6) {
+            document.getElementById('myButton2').innerText = 'serio mano, para de tentar';
+        }
+});
